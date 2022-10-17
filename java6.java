c899b24de9f52920884e972145ad9b5379f25bd7
Ptunc <spolentunc@gmail.com>
2022-10-17 16:31:00 +0300

    just commit

---
+++ java6.java
@@ -0,0 +1,21 @@
+package Java6;
+import java.util.Scanner;
+
+// (k) / (m) * (m)
+
+public class java6 {
+    public static void main(String[] args) {
+        float m, bmi;
+        int k;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("m cinsinden boyunuzu yazın: ");
+        m = input.nextFloat();
+        System.out.print("kilonuzu girin: ");
+        k = input.nextInt();
+
+        bmi = (k / (m * m));
+        System.out.println("BMI: " + bmi);
+
+    }
+}
